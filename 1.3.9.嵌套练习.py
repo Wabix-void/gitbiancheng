e5f766c5d27e9577eefd017ff607d0779143a3bd
Wabix-void <3235124677@qq.com>
2026-09-11 00:17:49 +0800

    小修改

--- 1.3.9.嵌套练习.py
+++ 1.3.9.嵌套练习.py
@@ -1,5 +1,5 @@
 import random
-num = random.randint(0,10)
+num = random.randint(1,10)
 print("猜字游戏，一共三次机会，请猜出答案")
 num_1 = int(input("请猜测数字"))
 if num_1 == num:
@@ -18,7 +18,7 @@ else:
         else:
             print("小了")
         num_3 = int(input("请第三次猜测数字"))
-    if num_3 == num:
-         print("恭喜你，终于猜对了")
-    else:
-        print("很遗憾，猜错了，答案是：%d"%num)
+        if num_3 == num:
+            print("恭喜你，终于猜对了")
+        else:
+            print("很遗憾，猜错了，答案是：%d"%num)
